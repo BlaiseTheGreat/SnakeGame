fc8e2aa924a3b33c435b609bdb2a1d6ee129ec60
Blaise Willoughby <blaise_willoughby@yahoo.com>
2021-07-20 22:05:26 -0500

    complete collision checking

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -55,6 +55,16 @@ public class GamePanel extends JPanel implements ActionListener {
 		}
 		g.setColor(Color.red);
 		g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
+		
+		for(int i = 0; i < bodyParts; i++) {
+			if(i == 0) {
+				g.setColor(Color.green);
+				g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+			} else {
+				g.setColor(new Color(45, 180, 0));
+				g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+			}
+		}
 	}
 	
 	public void newApple() {
@@ -63,7 +73,25 @@ public class GamePanel extends JPanel implements ActionListener {
 	}
 	
 	public void move() {
+		for(int i = bodyParts; i > 0; i--) {
+			x[i] = x[i - 1];
+			y[i] = y[i - 1];
+		}
 		
+		switch(direction) {
+		case 'U':
+			y[0] = y[0] - UNIT_SIZE;
+			break;
+		case 'D':
+			y[0] = y[0] + UNIT_SIZE;
+			break;
+		case 'L':
+			x[0] = x[0] - UNIT_SIZE;
+			break;
+		case 'R':
+			x[0] = x[0] + UNIT_SIZE;
+			break;
+		}
 	}
 	
 	public void checkApple() {
@@ -71,7 +99,36 @@ public class GamePanel extends JPanel implements ActionListener {
 	}
 	
 	public void checkCollisions() {
+		//this checks if head collides with body
+		for(int i = bodyParts; i > 0; i--) {
+			if((x[0] == x[i]) && (y[0] == y[i])) {
+				running = false;
+			}
+		}
 		
+		//checks if head collides with left border
+		if(x[0] < 0) {
+			running = false;
+		}
+		
+		//check if head collides with right border
+		if(x[0] > SCREEN_WIDTH - 1) {
+			running = false;
+		}
+		
+		//check if head touches top border
+		if(y[0] < 0) {
+			running = false;
+		}
+		
+		//check if head touches bottom border
+		if(y[0] > SCREEN_HEIGHT - 1) {
+			running = false;
+		}
+		
+		if(!running) {
+			timer.stop();
+		}
 	}
 	
 	public void gameOver(Graphics g) {
@@ -80,6 +137,12 @@ public class GamePanel extends JPanel implements ActionListener {
 	
 	@Override
 	public void actionPerformed(ActionEvent e) {
+		if(running) {
+			move();
+			checkApple();
+			checkCollisions();
+		}
+		repaint();
 		
 	}
 	
